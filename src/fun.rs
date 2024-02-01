be5db8fc0f6d8f7b02960c646d9bad129775d9c8
VlxtIykg <97310758+VlxtIykg@users.noreply.github.com>
2024-02-01 13:49:37 +0000

    Update stuff

--- src/fun.rs
+++ src/fun.rs
@@ -1,12 +1,20 @@
-struct Person{
-  name: String,
-  age: u8,
-  gender: String,
+pub struct Person{
+  pub name: String,
+  pub age: u8,
+  pub gender: String
+}
+
+impl Person{
+  fn print(&self){
+    println!("Name: {}", self.name);
+    println!("Age: {}", self.age);
+    println!("Gender: {}", self.gender);
+  }
+  fn change_name(&mut self, new_name: String){
+    self.name = new_name;
+  }
 }
 
 fn main(){
-  kami: Person;
-  kami.name = "Kami";
-  kami.age = 20;
-  kami.gender = "Male";
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-use std::time::Instant;
+mod fun;
 
-fn main() {
-    println!("Hello, world!");
+
+pub fn main() {
+    let mut kami = fun::Person{
+      name: String::from("Kami"),
+      age: age_gen(),
+      gender: String::from("yuh uh"),
+  
+    };
+  
+    let mut deftioon = fun::Person{
+      name: String::from("Deftioon"),
+      age: 255, // oh
+      gender: String::from("yuh uh"),
+    };
+  
+    deftioon.name = String::from("Deftiboob");
+    kami.name = String::from("Nyami")
+}
+
+
+fn age_gen() -> u8 {
+  println!("I am however old deftioon wants me to be!");    
+  return (12+21)/2; 
 }
